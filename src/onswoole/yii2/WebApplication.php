ff63ea3318a5d3eb7448201a4c55469d520f5637
Pan Wenbin <panwenbin@gmail.com>
2018-07-25 18:15:52 +0800

    尝试全部重置

--- src/onswoole/yii2/WebApplication.php
+++ src/onswoole/yii2/WebApplication.php
@@ -79,5 +79,40 @@ class WebApplication extends Application
         \yii\base\Widget::$counter = 0;
         \yii\caching\Dependency::resetReusableData();
         \yii\web\UploadedFile::reset();
+        \Yii::$app->errorHandler->unregister();
+        \Yii::getLogger()->dispatcher = null;
+        \Yii::setLogger(null);
+
+        if (isset(\Yii::$app->loadedModules['yii\debug\Module'])) {
+            $debug = \Yii::$app->loadedModules['yii\debug\Module'];
+            $debug->logTarget->module = null;
+            $debug->logTarget = null;
+            foreach ($debug->panels as $panel) {
+                $panel->module = null;
+            }
+            $debug->panels = [];
+        }
+
+        if (\Yii::$app->requestedAction) {
+            \Yii::$app->requestedAction->controller = null;
+            \Yii::$app->requestedAction = null;
+        }
+        if (\Yii::$app->controller) {
+            foreach (\Yii::$app->getModules() as $id => $module) {
+                if ($module instanceof \yii\base\Module) {
+                    $module->module = null;
+                }
+                \Yii::$app->setModule($id, null);
+            }
+            \Yii::$app->loadedModules = [];
+            \Yii::$app->controller->detachBehaviors();
+            \Yii::$app->controller->module = null;
+            \Yii::$app->controller->action = null;
+            \Yii::$app->controller = null;
+        }
+        foreach (\Yii::$app->components as $id => $component) {
+            \Yii::$app->clear($id);
+        }
+        \Yii::$container = new \yii\di\Container();
     }
 }
